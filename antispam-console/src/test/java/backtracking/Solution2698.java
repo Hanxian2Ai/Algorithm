3b84e6e00ae047ba2cd8d5e04c0b09e3a65dc8a2
HanXian <2389886090@qq.com>
2023-10-25 22:59:07 +0800

    add leetcode2698 solution

---
+++ antispam-console/src/test/java/backtracking/Solution2698.java
@@ -0,0 +1,34 @@
+package backtracking;
+
+class Solution2698 {
+
+    public static void main(String[] args) {
+        int i = punishmentNumber(10);
+        System.out.println(i);
+    }
+    public static int punishmentNumber(int n) {
+        int res = 0;
+
+        for (int i = 1; i <= n; i++) {
+            String s = Integer.toString(i * i);
+            if (backStacking(s,i,0, 0)){
+                res += Integer.parseInt(s);
+            }
+        }
+        return res;
+
+    }
+    public static boolean backStacking(String s, int target, int startIndex, int sum){
+        if (startIndex == s.length()){
+            return sum == target;
+        }
+        for (int i = startIndex; i < s.length(); i++) {
+            String substring = s.substring(startIndex, i + 1);
+            int valueItem = Integer.parseInt(substring);
+            if (backStacking(s, target,i + 1,sum + valueItem)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
