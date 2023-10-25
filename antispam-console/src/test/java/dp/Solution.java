73875d58053357d8b6c37602e7c6226ae3f98d76
HanXian <2389886090@qq.com>
2023-10-25 20:02:26 +0800

    add leetcode backtracking

---
+++ antispam-console/src/test/java/dp/Solution.java
@@ -0,0 +1,36 @@
+package dp;
+
+class Solution {
+    public static void main(String[] args) {
+        isSubsequence("abc","ahbgdc");
+    }
+    public static boolean isSubsequence(String s, String t) {
+        int a = s.length();
+        int b = s.length();
+        int[][] dp = new int[a][b];
+        boolean mark = false;
+
+        for(int i = 0; i < b; i++){
+            if(s.charAt(0) == t.charAt(i)){
+                dp[0][i] = 1;
+                mark = true;
+            }
+            if(mark == true){
+                dp[0][i] = 1;
+            }
+        }
+        
+        for(int i = 1; i < a; i++){
+            for(int j = 1; j < b; j++){
+                if(s.charAt(i) == t.charAt(j)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    dp[i][j] = dp[i][j-1];
+                }
+            }
+        }
+        return dp[a -1][b-1]  == a;
+
+
+    }
+}
