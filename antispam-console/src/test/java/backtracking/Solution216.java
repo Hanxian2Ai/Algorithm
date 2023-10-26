9437aa2c8fe2e531286f4530862c8e72a260f1d8
HanXian <2389886090@qq.com>
2023-10-26 22:17:55 +0800

    add leetcode solution216

---
+++ antispam-console/src/test/java/backtracking/Solution216.java
@@ -0,0 +1,34 @@
+package backtracking;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+class Solution216 {
+    LinkedList<Integer> path = new LinkedList<>();
+    List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        build(k, n, 1, 0);
+        return ans;
+    }
+
+    private void build(int k, int n, int startIndex, int sum) {
+
+        if (sum > n) return;
+
+        if (path.size() > k) return;
+
+        if (sum == n && path.size() == k) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+
+        for(int i = startIndex; i <= 9; i++) {
+            path.add(i);
+            sum += i;
+            build(k, n, i + 1, sum);
+            sum -= i;
+            path.removeLast();
+        }
+    }
+}
