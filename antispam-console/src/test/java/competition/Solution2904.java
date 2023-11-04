bc2db27ead9915e795f129191eb5c95b1818d648
HanXian <2389886090@qq.com>
2023-11-04 21:39:36 +0800

    add leetcode421solution

--- antispam-console/src/test/java/competition/Solution2904.java
+++ antispam-console/src/test/java/competition/Solution2904.java
@@ -44,12 +44,7 @@ class Solution2904 {
     }
 
     public String minStr(String a, String b) {
-        for (int i = 0; i < a.length(); i++) {
-            if (a.charAt(i) != b.charAt(i)) {
-                return a.charAt(i) == '0' ? a : b;
-            }
-        }
-        return a;
+        return a.compareTo(b) < 0 ? a : b;
     }
 
 }
