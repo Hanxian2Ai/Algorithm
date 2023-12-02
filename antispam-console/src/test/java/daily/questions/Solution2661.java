6a0bfcd22653cce10ffb660918fee470505c43f3
HanXian <2389886090@qq.com>
2023-12-02 22:53:19 +0800

    add leetcode daily solution

---
+++ antispam-console/src/test/java/daily/questions/Solution2661.java
@@ -0,0 +1,24 @@
+package daily.questions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution2661 {
+    public int firstCompleteIndex(int[] arr, int[][] mat) {
+        int n = mat.length, m = mat[0].length;
+        Map<Integer, int[]> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                map.put(mat[i][j], new int[]{i, j});
+            }
+        }
+        int[] c1 = new int[n], c2 = new int[m];
+        for (int i = 0; i < n * m; i++) {
+            int[] info = map.get(arr[i]);
+            int x = info[0], y = info[1];
+            if (++c1[x] == m || ++c2[y] == n) return i;
+        }
+        return -1;
+    }
+}
+
