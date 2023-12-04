34ec85f2932988aac894d9318febc86a5cca3e8f
HanXian <2389886090@qq.com>
2023-12-04 20:15:02 +0800

    add leetcode daily solution

--- antispam-console/src/test/java/daily/questions/Solution1038.java
+++ antispam-console/src/test/java/daily/questions/Solution1038.java
@@ -1,6 +1,20 @@
 package daily.questions;
 
 class Solution1038 {
+
+     public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+     }
+
     private int s = 0;
 
     public TreeNode bstToGst(TreeNode root) {
