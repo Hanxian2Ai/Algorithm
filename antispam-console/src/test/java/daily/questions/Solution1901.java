853790cb026882e1f1f7ff833815963ceb4279ee
HanXian <2389886090@qq.com>
2023-12-20 20:35:24 +0800

    add leetcode daily solution

--- antispam-console/src/test/java/daily/questions/Solution1901.java
+++ antispam-console/src/test/java/daily/questions/Solution1901.java
@@ -25,8 +25,3 @@ class Solution1901 {
         return idx;
     }
 }
-
-作者：灵茶山艾府
-链接：https://leetcode.cn/problems/find-a-peak-element-ii/solutions/2571587/tu-jie-li-yong-xing-zui-da-zhi-pan-duan-r4e0n/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
