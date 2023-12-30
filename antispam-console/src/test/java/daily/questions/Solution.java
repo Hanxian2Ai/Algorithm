27b20c17394b80c8c9b8a75c2762c28ca81813ed
HanXian <2389886090@qq.com>
2023-12-30 21:16:11 +0800

    add leetcode daily solution

--- antispam-console/src/test/java/daily/questions/Solution.java
+++ antispam-console/src/test/java/daily/questions/Solution.java
@@ -1,27 +1,31 @@
 package daily.questions;
 
+/**
+ * 1185. 一周中的第几天
+ * https://leetcode.cn/problems/day-of-the-week/description/
+ * 枚举模拟 闰年（400倍数 或者 4的倍数且不是100倍数）
+ */
 class Solution {
-    private static final int MOD = 1_000_000_007;
 
-    public int checkRecord(int n) {
-        long[][][] dp = new long[n + 1][2][3];
-        dp[1][0][0] = 1;
-        dp[1][0][1] = 1;
-        dp[1][1][0] = 1;
-        for (int i = 2; i < n; i++) {
-            dp[i][0][0] = (dp[i - 1][0][0] + dp[i - 1][0][1] + dp[i - 1][0][2]) % MOD;
-            dp[i][0][1] = dp[i- 1][0][0];
-            dp[i][0][2] = dp[i- 1][0][1];
-            dp[i][1][0] = (dp[i - 1][1][0] + dp[i - 1][1][1] + dp[i - 1][1][2] + dp[i - 1][0][0] + dp[i - 1][0][1] + dp[i - 1][0][2]) % MOD;
-            dp[i][1][1] = dp[i -1][1][0];
-            dp[i][1][2] = dp[i- 1][1][1];
+    static String[] week = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    static int[] nums = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public String dayOfTheWeek(int day, int month, int year) {
+        int days = 4;
+        for(int i = 1971; i < year; i++){
+            days += (i % 400 == 0 || (i % 4 == 0 && i % 100 != 0)) ? 366 : 365;
         }
-        long ans = 0;
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 3; j++) {
-                ans = (ans + dp[n - 1][i][j]) % MOD;
+
+        for(int i = 1; i < month; i++){
+            boolean isLeak  = (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0));
+            if(i == 2){
+                days += isLeak ? 29 : 28;
+            }else{
+                days += nums[i - 1];
             }
         }
-        return (int) ans;
+
+        days += day;
+        return week[days % 7];
     }
 }
