5d7befb4c5db224a1a1f998b3f715c7030601960
HanXian <2389886090@qq.com>
2024-01-01 20:35:54 +0800

    add leetcode daily solution

--- antispam-console/src/test/java/daily/questions/Solution.java
+++ antispam-console/src/test/java/daily/questions/Solution.java
@@ -1,31 +1,55 @@
 package daily.questions;
 
+import java.util.ArrayList;
+
 /**
- * 1185. 一周中的第几天
- * https://leetcode.cn/problems/day-of-the-week/description/
- * 枚举模拟 闰年（400倍数 或者 4的倍数且不是100倍数）
+ * 1599. 经营摩天轮的最大利润
+ * https://leetcode.cn/problems/maximum-profit-of-operating-a-centennial-wheel/description/
+ * 蠢比模拟题 这辈子不做第二遍
  */
 class Solution {
-
-    static String[] week = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-    static int[] nums = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-    public String dayOfTheWeek(int day, int month, int year) {
-        int days = 4;
-        for(int i = 1971; i < year; i++){
-            days += (i % 400 == 0 || (i % 4 == 0 && i % 100 != 0)) ? 366 : 365;
-        }
-
-        for(int i = 1; i < month; i++){
-            boolean isLeak  = (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0));
-            if(i == 2){
-                days += isLeak ? 29 : 28;
+    public static int minOperationsMaxProfit(int[] customers, int boardingCost, int runningCost) {
+        int holdPeople = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < customers.length; i++) {
+            holdPeople += customers[i];
+            if (holdPeople <= 4){
+                list.add(holdPeople);
+                holdPeople = 0;
             }else{
-                days += nums[i - 1];
+                holdPeople -=4;
+                list.add(4);
             }
         }
+        if (holdPeople != 0){
+            for (int i = 0; i < holdPeople / 4; i++) {
+                list.add(4);
+            }
+            if (holdPeople % 4 != 0){
+                list.add(holdPeople % 4);
+            }
+        }
+        int maxProfit = 0;
+        int sum = 0;
+        int running = 0;
+        int min = 0;
 
-        days += day;
-        return week[days % 7];
+        for (int i = 0; i < list.size(); i++) {
+            sum += list.get(i);
+            running++;
+            int now = (sum * boardingCost - running * runningCost);
+            if (now > maxProfit){
+                maxProfit = now;
+                min = running;
+            }
+        }
+        if (maxProfit <= 0){
+            return  -1;
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        minOperationsMaxProfit(new int[]{1,2,1,2,1,2,1,1}, 90, 46);
     }
 }
