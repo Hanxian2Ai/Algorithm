edff6569cca297f8754939771bb879708473c957
HanXian <2389886090@qq.com>
2024-01-11 19:48:38 +0800

    add leetcode daily solution

---
+++ algorithm-leetcode/src/main/java/com/hanxian/algorithm/leetcode/questions/Solution2645.java
@@ -0,0 +1,18 @@
+package com.hanxian.algorithm.leetcode.questions;
+
+/**
+ * 2645. 构造有效字符串的最少插入数
+ *  https://leetcode.cn/problems/minimum-additions-to-make-valid-string/description/
+ * 方法一：考虑相邻字母
+ */
+class Solution2645 {
+    public int addMinimum(String word) {
+        char[] s = word.toCharArray();
+        int ans = s[0] + 2 - s[s.length - 1];
+        for (int i = 1; i < s.length; i++) {
+            ans += (s[i] + 2 - s[i - 1]) % 3;
+        }
+        return ans;
+    }
+}
+
