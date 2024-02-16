9b43b1a444dc23adf8876e092e90628b2bb573cc
HanXian <2389886090@qq.com>
2024-02-16 19:26:56 +0800

    add leetcode daily solution

--- algorithm-leetcode/src/main/java/com/hanxian/algorithm/leetcode/daily/questions/Solution102.java
+++ algorithm-leetcode/src/main/java/com/hanxian/algorithm/leetcode/daily/questions/Solution102.java
@@ -3,6 +3,7 @@ package com.hanxian.algorithm.leetcode.daily.questions;
 import com.hanxian.algorithm.leetcode.daily.questions.Solution1457.TreeNode;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,24 +15,32 @@ class Solution102 {
     public List<List<Integer>> levelOrder(TreeNode root) {
         LinkedList<TreeNode> queue = new LinkedList<>();
         List<List<Integer>> res = new ArrayList<>();
-        if (root == null){
+        if (root == null) {
             return res;
         }
         queue.add(root);
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int size = queue.size();
             List<Integer> resLevel = new ArrayList<>(size);
             for (int i = 0; i < size; i++) {
                 TreeNode poll = queue.poll();
                 resLevel.add(poll.val);
-                if (poll.left != null){
+                if (poll.left != null) {
                     queue.offer(poll.left);
                 }
-                if (poll.right != null){
+                if (poll.right != null) {
                     queue.offer(poll.right);
                 }
             }
             res.add(resLevel);
+        }
+
+        for (int i = 0; i < res.size(); i++) {
+            if (((i + 1) & 1) == 0) {
+                List<Integer> item = res.get(i);
+                Collections.reverse(item);
+                res.set(i, item);
+            }
         }
         return res;
     }
